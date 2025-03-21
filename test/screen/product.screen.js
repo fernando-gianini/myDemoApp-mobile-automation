a610a425bf663db8db38825acb1399157b17ed94
Fernando Gianini <fernando.gianini.pro@gmail.com>
2025-03-20 22:40:21 -0300

    finish test product and CI WorkFlow Created

--- test/screen/product.screen.js
+++ test/screen/product.screen.js
@@ -9,14 +9,30 @@ class Product {
         this.primaryAscendingPrice = "//android.widget.TextView[@content-desc='store item price' and @text='$7.99']"
         this.secondaryAscendingPrice = "//android.widget.TextView[@content-desc='store item price' and @text='$9.99']"
         this.filterDescendingName = "//android.widget.TextView[@text='Name - Descending']"
-        this.primaryDescendingName = "//android.widget.TextView[@content-desc='store item text' and @text='Sauce Labs Fleece Jacket']"
-        this.secondaryDescendingName = "//android.widget.TextView[@content-desc='store item text' and @text='Sauce Labs Backpack']"
+        this.primaryDescendingName = "//android.widget.TextView[@content-desc='store item text' and @text='Test.allTheThings() T-Shirt']"
+        this.secondaryDescendingName = "//android.widget.TextView[@content-desc='store item text' and @text='Sauce Labs Onesie']"
         this.frameworkLayout = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout"
         this.cartButton = "//android.view.ViewGroup[@content-desc='cart badge']/android.widget.ImageView"
         this.productText = "//android.widget.TextView[@text='Products']"
         this.picturePrimaryProduct = "(//android.view.ViewGroup[@content-desc='store item'])[1]/android.view.ViewGroup[1]/android.widget.ImageView"
         this.pictureSecundaryProduct = "(//android.view.ViewGroup[@content-desc='store item'])[2]/android.view.ViewGroup[1]/android.widget.ImageView"
-        this.secundaryNameProduct = "//android.widget.TextView[@content-desc='store item text' and @text='Sauce Labs Bike Light']"
+        this.secondaryNameProduct = "//android.widget.TextView[@content-desc='store item text' and @text='Sauce Labs Bike Light']"
+        this.primaryAscendingName = "//android.widget.TextView[@content-desc='store item text' and @text='Sauce Labs Backpack']"
+        this.filterAscendingButton = "//android.widget.TextView[@text='Name - Ascending']"   
+        this.primaryCountainerProduct = "(//android.view.ViewGroup[@content-desc='store item'])[1]"
+        this.frameworkLayoutProduct = "//android.widget.ScrollView[@content-desc='product screen']/android.view.ViewGroup"
+        this.priceCountainer = "//android.widget.TextView[@content-desc='product price']"
+        this.addToCartButton = "//android.view.ViewGroup[@content-desc='Add To Cart button']"
+        this.pictureProduct = "//android.widget.ScrollView[@content-desc='product screen']/android.view.ViewGroup/android.widget.ImageView"
+        this.productName = "//android.view.ViewGroup[@content-desc='container header']"
+        this.selectColorButton = "//android.view.ViewGroup[@content-desc='blue circle']"
+        this.descritionProduct = "//android.widget.TextView[@content-desc='product description']"
+        this.avaliationProductButton = "//android.view.ViewGroup[@content-desc='review star 3']"
+        this.confirmModalButton = "//android.view.ViewGroup[@content-desc='Close Modal button']"
+        this.plusProductButton = "//android.view.ViewGroup[@content-desc='counter plus button']/android.widget.ImageView"
+        this.counterAmountProduct = "//android.view.ViewGroup[@content-desc='counter amount']"
+        this.minusProductButton = "//android.view.ViewGroup[@content-desc='counter minus button']/android.widget.ImageView"
+        this.productPlusInCart = "(//android.widget.TextView[@text='1'])[1]"
     }
 
 
@@ -30,40 +46,73 @@ async viewElementsProductPage() {
     await $(this.secondaryDescendingPrice).isDisplayed()
     await $(this.secondaryAscendingPrice).isDisplayed()
     await $(this.secondaryDescendingName).isDisplayed()
-    await $(this.secundaryNameProduct).isDisplayed()
+    await $(this.secondaryNameProduct).isDisplayed()
 }
 
 async filtreDescendingPrice() {
-    await $(this.filterButton).click();
-    await $(this.filterPriceDecentent).click();
+    await $(this.filterButton).click()
+    await $(this.filterPriceDecentent).click()
     await browser.pause(2000);
-    await $(this.primaryDescendingPrice).getText();
-    await $(this.secondaryDescendingPrice).getText();   
-    const primaryPrice = parseFloat((await $(this.primaryDescendingPrice).getText()).replace('$', ''));
-    const secondaryPrice = parseFloat((await $(this.secondaryDescendingPrice).getText()).replace('$', '')); 
-    expect(primaryPrice).toBeGreaterThan(secondaryPrice);
+    await $(this.primaryDescendingPrice).getText()
+    await $(this.secondaryDescendingPrice).getText() 
+    const primaryPrice = parseFloat((await $(this.primaryDescendingPrice).getText()).replace('$', ''))
+    const secondaryPrice = parseFloat((await $(this.secondaryDescendingPrice).getText()).replace('$', ''))
+    expect(primaryPrice).toBeGreaterThan(secondaryPrice)
     }
 
 async filtreAscendingPrice() {
-    await $(this.filterButton).click();
-    await $(this.filtrePriceAscending).click();
-    await browser.pause(2000);
-    await $(this.primaryAscendingPrice).getText();
-    await $(this.secondaryAscendingPrice).getText();
-    const primaryPrice = parseFloat((await $(this.primaryAscendingPrice).getText()).replace('$', ''));
-    const secondaryPrice = parseFloat((await $(this.secondaryAscendingPrice).getText()).replace('$', ''));
-    expect(primaryPrice).toBeLessThan(secondaryPrice);
+    await $(this.filterButton).click()
+    await $(this.filtrePriceAscending).click()
+    await browser.pause(2000)
+    await $(this.primaryAscendingPrice).getText()
+    await $(this.secondaryAscendingPrice).getText()
+    const primaryPrice = parseFloat((await $(this.primaryAscendingPrice).getText()).replace('$', ''))
+    const secondaryPrice = parseFloat((await $(this.secondaryAscendingPrice).getText()).replace('$', ''))
+    expect(primaryPrice).toBeLessThan(secondaryPrice)
     }
 
- async filtreDescendingName() {
-    await $(this.filterButton).click();
-    await $(this.filterDescendingName).click();
-    await browser.pause(2000);
-    await $(this.primaryDescendingName).getText();
-    await $(this.secondaryDescendingName).getText();
-    expect(primaryName).localeCompare(secondaryName);
+async filtreDescendingName() {
+    await $(this.filterButton).click()
+    await $(this.filterDescendingName).click()
+    await browser.pause(2000)
+    const primaryName = await $(this.primaryDescendingName).getText()
+    const secondaryName = await $(this.secondaryDescendingName).getText()
+    const comparison = primaryName.localeCompare(secondaryName)
+    expect(comparison).toBeGreaterThan(0)
     }
 
+async filtreAscendingName() {
+    await $(this.filterButton).click()
+    await $(this.filterAscendingButton).click()
+    await browser.pause(2000)
+    const primaryName = await $(this.primaryAscendingName).getText()
+    const secondaryName = await $(this.secondaryNameProduct).getText()
+    const comparison = primaryName.localeCompare(secondaryName)
+    expect(comparison).toBeLessThan(0)
+    }
+
+async viewElementsProductPage() {
+    await $(this.primaryCountainerProduct).click()
+    await $(this.frameworkLayoutProduct).isDisplayed()
+    await $(this.priceCountainer).isDisplayed()
+    await $(this.addToCartButton).isDisplayed()
+    await $(this.pictureProduct).isDisplayed()
+    await $(this.productName).getText()
+    await $(this.selectColorButton).isDisplayed()
+    await $(this.descritionProduct).isDisplayed()
+    await $(this.avaliationProductButton).isDisplayed()
+    }
+async testButtonsProductPage() {
+    await $(this.selectColorButton).click()
+    await $(this.avaliationProductButton).click()
+    await $(this.confirmModalButton).click()
+    await $(this.plusProductButton).click()
+    await $(this.counterAmountProduct).getText('2')
+    await $(this.minusProductButton).click()
+    await $(this.counterAmountProduct).getText('1')
+    await $(this.addToCartButton).click()   
+    await $(this.productPlusInCart).getText('1')
+    }
 }
 
 export default new Product()
--- test/specs/product.e2e.js
+++ test/specs/product.e2e.js
@@ -7,29 +7,53 @@ import menuScreen from '../screen/menu.screen.js'
 import productScreen from '../screen/product.screen.js'
 
 
-describe('My product application', () => {
+describe('My Filter product application', () => { // Todos os comentados dessa seção rodam individualmente utilizando .only
     it('Detect elements product page', async () => {
         await menuScreen.accessLoginScreen()
         await loginScreen.fillLoginForm(userData.userSuccess.username, userData.userSuccess.password)
-        await productScreen.viewElementsProductPage()
+        await productScreen.viewElementsCatalogPage()
     })
 
     it('You must access the Product Screen and use the filter Descending Price', async () => {
-        await menuScreen.accessLoginScreen()
-        await loginScreen.fillLoginForm(userData.userSuccess.username, userData.userSuccess.password)
+        //await menuScreen.accessLoginScreen()
+        //await loginScreen.fillLoginForm(userData.userSuccess.username, userData.userSuccess.password)
         await productScreen.filtreDescendingPrice()
     })
 
-    it.only('You must access the Product Screen and use the filter Ascending Price', async () => {
-        await menuScreen.accessLoginScreen()
-        await loginScreen.fillLoginForm(userData.userSuccess.username, userData.userSuccess.password)
+    it('You must access the Product Screen and use the filter Ascending Price', async () => {
+        //await menuScreen.accessLoginScreen()
+        //await loginScreen.fillLoginForm(userData.userSuccess.username, userData.userSuccess.password)
         await productScreen.filtreAscendingPrice()
     })
 
     it('You must access the Product Screen and use the filter Descending Name', async () => {
-        await menuScreen.accessLoginScreen()
-        await loginScreen.fillLoginForm(userData.userSuccess.username, userData.userSuccess.password)
-        await navigationScreen.viewElementsPrimaryPage()
+        //await menuScreen.accessLoginScreen()
+        //await loginScreen.fillLoginForm(userData.userSuccess.username, userData.userSuccess.password)
+        //await navigationScreen.viewElementsPrimaryPage()
         await productScreen.filtreDescendingName()
     })
+
+    it('You must access the Product Screen and use the filter Descending/Ascending Name', async () => {
+        //await menuScreen.accessLoginScreen()
+        //await loginScreen.fillLoginForm(userData.userSuccess.username, userData.userSuccess.password)
+        //await navigationScreen.viewElementsPrimaryPage()
+        await productScreen.filtreDescendingName()
+        await productScreen.filtreAscendingName()
+    })
+})
+
+describe('My product application', () => {
+    it('You must access produtc page and details product', async () => {
+        //await menuScreen.accessLoginScreen()
+        //await loginScreen.fillLoginForm(userData.userSuccess.username, userData.userSuccess.password)
+        await navigationScreen.viewElementsPrimaryPage()
+        await productScreen.viewElementsProductPage()
+    })
+    it('You must access produtc page and test buttons', async () => {
+        //await menuScreen.accessLoginScreen()
+        //await loginScreen.fillLoginForm(userData.userSuccess.username, userData.userSuccess.password)
+        //await productScreen.viewElementsProductPage()
+        await productScreen.testButtonsProductPage()
+    })
+
 })
